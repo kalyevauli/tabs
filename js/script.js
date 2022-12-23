8d9499d70b8194c909b2c0cc162a329f6f43bb87
kalyevauli <toktosunovaurkuya003@gmail.com>
2022-12-23 22:04:52 +0300

    tabs

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+const btn=document.querySelector(".title-btn button")
+btn.addEventListener("click",()=>{
+    document.querySelector(".title-modal").style.display="block"
+})
+const btnClose=document.querySelector(".title-modal-bg-square-close")
+btnClose.addEventListener("click",()=>{
+    document.querySelector(".title-modal").style.display="none"
+})
